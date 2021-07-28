91dbc31aca62109d486a59cfc7df133e8671254e
Phil Fearon <pgfearo@gmail.com>
2021-07-28 11:18:08 +0100

    add closeXBooks function - but dont call yet

--- src/extension.ts
+++ src/extension.ts
@@ -21,16 +21,32 @@ export function activate(context: vscode.ExtensionContext) {
 
 	ExtensionData.initEditor(vscode.window.activeTextEditor);
 
-  context.subscriptions.push(
+	context.subscriptions.push(
 		vscode.languages.registerDocumentSemanticTokensProvider({ language: 'json' }, new XpathResultTokenProvider(), XpathResultTokenProvider.getLegend()),
 		//vscode.languages.registerDefinitionProvider({ language: 'javascript' }, new JsonDefinitionProvider()),
 		//vscode.languages.registerHoverProvider({ language: 'javascript' }, new JSONHoverProvider()),
 		vscode.window.onDidChangeActiveTextEditor(editor => {
-				ExtensionData.registerEditor(editor);
+			ExtensionData.registerEditor(editor);
 		}),
 		vscode.languages.registerDocumentLinkProvider({ language: 'json' }, new LinkProvider()),
 		vscode.notebooks.registerNotebookCellStatusBarItemProvider('xbook', new CellStatusProvider()),
-    vscode.workspace.registerNotebookSerializer('xbook', new XBookSerializer()),
-    new XBookController()
-  );
+		vscode.workspace.registerNotebookSerializer('xbook', new XBookSerializer()),
+		new XBookController()
+	);
+}
+
+async function closeXBooks() {
+	// attempt to close XBook as there's a renderer problem if it's still open 
+	// when restarting vscode
+	const editors = vscode.window.visibleTextEditors;
+	for (let index = 0; index < editors.length; index++) {
+		const editor = editors[index];
+		if (editor.document.uri.fsPath.endsWith('.xbook')) {
+			await vscode.window.showTextDocument(editor.document.uri, { preview: true, preserveFocus: false });
+			await vscode.commands.executeCommand('workbench.action.closeActiveEditor');				
+		}
+	}}
+
+export async function deactivate() {
+	// await closeXBooks();
 }
